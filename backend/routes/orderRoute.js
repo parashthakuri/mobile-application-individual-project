58ac4a326bd75326a0bdb9f55256ec4702237267
Ashok Shrestha <105191017+parashthakuri@users.noreply.github.com>
2023-07-15 14:01:45 +0545

    routes

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
+} = require("../controller/oderController");
+const router = express.Router();
+const {
+  isAuthenticatedUser,
+  autherorizeRole,
+  auth,
+} = require("../middleware/auth");
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/for/user").post(auth, newOrder);
+
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, autherorizeRole("admin"),getAllOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, autherorizeRole("admin"), updateOrder)
+  .delete(isAuthenticatedUser, autherorizeRole("admin"), deleteOrder);
+
+
+  
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+router.route("/orders/me/user").get(auth, myOrders);
+module.exports = router;
